d0a49e85b09eea422417ee57242edf7d9e81e1f8
Jess Sullivan <Jess@sulliwood.org>
2026-05-26 13:23:08 -0400

    oauth-mux 0.1.11

--- Formula/oauth-mux.rb
+++ Formula/oauth-mux.rb
@@ -1,26 +1,26 @@
 class OauthMux < Formula
   desc "OAuth fallback muxing for AI harness subscriptions"
   homepage "https://omux.xoxd.ai"
+  version "0.1.11"
   license "MIT"
-  version "0.1.10"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Jesssullivan/oauth-mux/releases/download/v0.1.10/oauth-mux-aarch64-macos.tar.gz"
-      sha256 "8a1e3516fabaec9a3b518f38db77ad4240c30f7809ca98ed1052e0e8824fdc7d"
+      url "https://github.com/Jesssullivan/oauth-mux/releases/download/v0.1.11/oauth-mux-aarch64-macos.tar.gz"
+      sha256 "62da7b5f3b12d1e617158f690ba4534555b9f145fabd89475816d3191f0215ae"
     else
-      url "https://github.com/Jesssullivan/oauth-mux/releases/download/v0.1.10/oauth-mux-x86_64-macos.tar.gz"
-      sha256 "465a285434b2429fcc57d627522b13e6d1c425b1869f87c4c999739f3e346bbf"
+      url "https://github.com/Jesssullivan/oauth-mux/releases/download/v0.1.11/oauth-mux-x86_64-macos.tar.gz"
+      sha256 "82f3cf433171a1a499f34fa1c89f9bcafbaa1cc0ab40a253cab9e7a18e325bdf"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/Jesssullivan/oauth-mux/releases/download/v0.1.10/oauth-mux-aarch64-linux.tar.gz"
-      sha256 "ab29c88c61e3e2da1eb4624d0403facab6a311104326b79d17f4053af8cb3e6e"
+      url "https://github.com/Jesssullivan/oauth-mux/releases/download/v0.1.11/oauth-mux-aarch64-linux.tar.gz"
+      sha256 "0294f78cab353387f3b4b9b359fd6f2a2f08333588f2bb013ce300dcd14d6175"
     else
-      url "https://github.com/Jesssullivan/oauth-mux/releases/download/v0.1.10/oauth-mux-x86_64-linux.tar.gz"
-      sha256 "b55635c10a034be307295b2b2f7d43501437efb0975662613866a61ccf54c800"
+      url "https://github.com/Jesssullivan/oauth-mux/releases/download/v0.1.11/oauth-mux-x86_64-linux.tar.gz"
+      sha256 "6555dc79a986fdea2219538c02e593b2292dc0761f1652ace24e7d2a58dc3f60"
     end
   end
 
